f3cace700dc05c672c43a7b34c520af61cda7825
Nikita-GIF110 <65663981+Nikita-GIF110@users.noreply.github.com>
2021-08-03 18:54:55 +0300

    Add files via upload

---
+++ scripts/dist/main.dev.js
@@ -0,0 +1,66 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  var anchorLinks = document.querySelectorAll('.header_navigation_list_link a');
+
+  for (var i = 0; i < anchorLinks.length; i++) {
+    anchorLinks[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockId = e.target.getAttribute('href').substr(1);
+      document.getElementById(blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    });
+  }
+
+  var menuBtn = document.querySelector('.m-menu-btn');
+  var header = document.querySelector('.header');
+  menuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    menuBtn.classList.toggle('show');
+    header.classList.toggle('show');
+  });
+  var contentLinks = document.querySelectorAll('.navigation_link');
+  var contentTabs = document.querySelectorAll('.block');
+  contentLinks.forEach(function (item) {
+    item.addEventListener('click', function (e) {
+      e.preventDefault();
+      var currentLink = item;
+      var linkId = currentLink.getAttribute('data-tab');
+      var currentTab = document.querySelector(linkId);
+
+      if (!currentLink.classList.contains('active')) {
+        contentLinks.forEach(function (item) {
+          item.classList.remove('active');
+        });
+        contentTabs.forEach(function (item) {
+          item.classList.remove('active');
+        });
+        currentLink.classList.add('active');
+        currentTab.classList.add('active');
+      }
+    });
+  });
+  var accordionLinks = document.querySelectorAll('.accordion_item_link');
+
+  var _loop = function _loop(_i) {
+    accordionLinks[_i].addEventListener('click', function (e) {
+      e.preventDefault();
+
+      if (!accordionLinks[_i].classList.contains('active')) {
+        for (var _i2 = 0; _i2 < accordionLinks.length; _i2++) {
+          accordionLinks[_i2].parentElement.classList.remove('active');
+        }
+
+        accordionLinks[_i].parentElement.classList.add('active');
+      }
+    });
+  };
+
+  for (var _i = 0; _i < accordionLinks.length; _i++) {
+    _loop(_i);
+  }
+
+  document.querySelector('.accordion_item_link').click();
+});
---
+++ scripts/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const anchorLinks = document.querySelectorAll('.header_navigation_list_link a');
+  for(let i = 0; i <anchorLinks.length; i++) {
+    anchorLinks[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      const blockId = e.target.getAttribute('href').substr(1);
+      document.getElementById(blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
+  
+  const menuBtn = document.querySelector('.m-menu-btn');
+  const header = document.querySelector('.header');
+
+  menuBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    menuBtn.classList.toggle('show');
+    header.classList.toggle('show');
+  })
+
+  const contentLinks = document.querySelectorAll('.navigation_link');
+  const contentTabs = document.querySelectorAll('.block');
+
+  contentLinks.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+
+      e.preventDefault();
+      let currentLink = item;
+      let linkId = currentLink.getAttribute('data-tab');
+      let currentTab = document.querySelector(linkId);
+
+      if(!currentLink.classList.contains('active')){
+         contentLinks.forEach(function(item) {
+          item.classList.remove('active');
+        });
+        contentTabs.forEach(function(item) {
+          item.classList.remove('active');
+        });
+
+        currentLink.classList.add('active');
+        currentTab.classList.add('active');
+      }
+    })
+  });
+
+  const accordionLinks = document.querySelectorAll('.accordion_item_link');
+  
+  for(let i = 0; i < accordionLinks.length; i++){
+    accordionLinks[i].addEventListener('click', function(e) {
+      e.preventDefault();
+      if(!accordionLinks[i].classList.contains('active')){
+        for(let i = 0; i < accordionLinks.length; i++){
+          accordionLinks[i].parentElement.classList.remove('active');
+        }
+        accordionLinks[i].parentElement.classList.add('active');
+      }
+    })
+  }
+  document.querySelector('.accordion_item_link').click();
+
+});
